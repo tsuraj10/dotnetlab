fae7833b95c9bfd2c13d1145802988593daf4e0d
tsuraj10 <103104631+tsuraj10@users.noreply.github.com>
2023-04-02 07:53:40 +0545

    dotnet

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            overloading cow=new overloading();
+            cow.WhatIEat();
+            cow.WhatIEat("grass");
+            cow.WhatIEat("grass", "straw");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ polymorphism/polymorphism/overloading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class overloading
+    {
+            public void WhatIEat()
+            {
+                Console.WriteLine("Cow eat grass");
+            }
+            public void  WhatIEat(string food)
+            {
+                Console.WriteLine("Cow eat "+ food);
+            }
+            public void WhatIEat(string food1, string food2)
+            {
+                Console.WriteLine("Cow eat " + food1 +" and "+ food2);
+            }
+    }
+}

e7ec47ab68262790adf86b5d7e6b35149a743abc
tsuraj10 <103104631+tsuraj10@users.noreply.github.com>
2023-04-04 08:20:19 +0545

    dotnet

---
+++ synchronous/synchronous/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace synchronous
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            function2();
+            function1();
+            
+            Console.ReadKey();
+        }
+        public static void function1()
+        {
+            for(int counter=0;counter<=50;counter++)
+            {
+                Console.WriteLine("I am from function1");
+            }
+        }
+        public static async void function2()
+        {
+            await Task.Run(() =>
+            {
+                for (int counter = 0; counter <=50; counter++)
+                {
+                    Console.WriteLine("I am from function2");
+                }
+            });
+        }
+    }
+}
